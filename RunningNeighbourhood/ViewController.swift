6d982c7b1ab2cb3b29353873aa4f998d8d094761
Asya Sher <asia.kondairy@gmail.com>
2023-03-02 19:23:22 +0600

    Initial Commit

---
+++ RunningNeighbourhood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunningNeighbourhood
+//
+//  Created by Asya Sher on 02.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
